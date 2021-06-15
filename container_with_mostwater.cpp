b48757808372e5f2b57f5b8e5da09d7a26deca56
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2021-06-15 22:17:27 +0530

    Add files via upload

---
+++ container_with_mostwater.cpp
@@ -0,0 +1,40 @@
+//Container with most water
+#include<iostream>
+#include<vector>
+using namespace std;
+ int maxArea(vector<int>&vec)
+ {
+     int l=0;
+     int r=vec.size()-1;
+     int max_area=0;
+     while(l<r)
+     {
+         int lh=vec[l];
+         int rh=vec[r];
+         int length=r-l;
+         int breadth=min(lh,rh);
+         int curr_area=length*breadth;
+         int max_area=max(curr_area,max_area);
+    }
+    return max_area;
+}
+int main()
+{
+    vector<int>vec;
+    int n;
+
+    cout<<"Enter numberof elements : ";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int x;
+        cin>>x;
+        vec.push_back(x);
+    }
+    cout<<"Given numbers are : ";
+    for(auto c:vec)
+    cout<<c<<" ";
+    cout<<"Maximum area is : "<<maxArea(vector<int>&vec);
+    
+    
+}
