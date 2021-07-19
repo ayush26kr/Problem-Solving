c9271515e44e9e364eea3322cb7d099e3f9e066a
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2021-07-19 22:58:47 +0530

    Add files via upload

---
+++ min_jumps_to_reach_end_of_array.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+ int minJumps(int arr[], int n){
+        if(n==1)
+        return 0;
+        int res=INT_MAX;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(i+arr[i]>=n-1)
+            {
+                int sub_res=minJumps(arr,i+1);
+                if(sub_res!=INT_MAX)
+                res=min(res,sub_res+1);
+            }
+        }
+        return res;
+ }
+int main()
+{
+    int arr[] = {1, 4, 3, 2, 6, 7};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    cout<<minJumps(arr,n);
+}
