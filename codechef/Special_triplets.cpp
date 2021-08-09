ea83329961b3367da50b0a94cabc3f57c83a8060
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2021-08-09 20:11:14 +0530

    Special Triplets

---
+++ codechef/Special_triplets.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,triplets=0;
+        cin>>n;
+        for(int c=1;c<=n;c++)
+        {
+            for(int b=c;b<=n;b+=c)
+            {
+                if(b%c==0)
+                {
+                    for(int a=c;a<=n;a+=b)
+                    {
+                        if(a%b==c)
+                        triplets++;
+                    }
+                }
+            }
+
+        }
+        cout<<triplets<<endl;
+    }
+}
